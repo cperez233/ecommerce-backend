3326e4602fff4a4705f95e63acfa05df34b35a93
Cristian Perez <cperez233@unab.edu.co>
2025-09-09 13:58:36 -0500

    Rutas de listas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/products', function () {
+    return "List products";
+});
+
+Route::get('/products/{id}/{category?}', function ($id, $category = null) {
+    if ($category != null) {
+        return "Detail product: " . $id . " with category: " . $category;
+    } else {
+        return "Detail product: " . $id;
+    }
 });
